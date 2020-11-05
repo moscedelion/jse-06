b69b8f7b77106ff0cf041fa0a21e35f15b00a038
asiman gasanov <gasanov_ad@nlmk.com>
2020-11-06 01:53:36 +0500

    Added eternal loop

--- src/main/java/ru/gasanov/tm/App.java
+++ src/main/java/ru/gasanov/tm/App.java
@@ -1,5 +1,7 @@
 package ru.gasanov.tm;
 
+
+import java.util.Scanner;
 import static ru.gasanov.tm.constant.TerminalConst.*;
 
 public class App {
@@ -7,18 +9,30 @@ public class App {
     public static void main(final String[] args) {
         run(args);
         displayWelcome();
+        final Scanner scanner = new Scanner(System.in);
+        String command  = "";
+        while (!CMD_EXIT.equals(command)){
+            command = scanner.nextLine();
+            run(command);
+        }
     }
 
     private static void run(final String[] args) {
         if (args == null) return;
-        if (args.length <1) return;
+        if (args.length < 1) return;
         final String param = args[0];
+        final int result = run(param);
+        System.exit(result);
+    }
 
+    private static int run(final String param) {
+        if (param == null) return -1;
         switch (param) {
-            case CMD_HELP: displayHelp();
-            case CMD_ABOUT: displayAbout();
-            case CMD_VERSION: displayVersion();
-            default: displayError();
+            case CMD_HELP: return displayHelp();
+            case CMD_ABOUT: return displayAbout();
+            case CMD_VERSION: return displayVersion();
+            case CMD_EXIT: return displayExit();
+            default: return displayError();
         }
     }
 
@@ -26,28 +40,32 @@ public class App {
         System.out.println("* WELCOME TO TASK MANAGER *");
     }
 
+    public static int displayExit() {
+        System.out.println(" Terminate programm");
+        return 0;
+    }
 
-    private static void  displayAbout() {
+    private static int  displayAbout() {
         System.out.println("Gasanov Asiman");
         System.out.println("gasanov_ad@nlmk.com");
-        System.exit(0);
+        return 0;
     }
 
-    private static  void displayError() {
+    private static  int displayError() {
         System.out.println("ERROR! You passed wrong parameters ");
-        System.exit(-1);
+        return -1;
     }
 
-    private static void  displayHelp() {
+    private static int  displayHelp() {
         System.out.println("version - Display program version.");
         System.out.println("about - Display programm info.");
         System.out.println("help - Display list of terminal commands.");
-        System.exit(0);
+        return 0;
     }
 
-    private static void  displayVersion() {
+    private static int  displayVersion() {
         System.out.println("1.0.0");
-        System.exit(0);
+        return 0;
     }
 
 }
