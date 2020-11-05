f3b21bee4ce014e7c570060a6b7f280fb02682d0
asiman gasanov <gasanov_ad@nlmk.com>
2020-11-06 00:37:17 +0500

    added jse-02 app code

--- src/main/java/ru/gasanov/tm/App.java
+++ src/main/java/ru/gasanov/tm/App.java
@@ -1,8 +1,53 @@
 package ru.gasanov.tm;
 
+import static ru.gasanov.tm.constant.TerminalConst.*;
+
 public class App {
 
-    public static void main(String[] args) {
-        System.out.println("hello world");
+    public static void main(final String[] args) {
+        run(args);
+        displayWelcome();
     }
+
+    private static void run(final String[] args) {
+        if (args == null) return;
+        if (args.length <1) return;
+        final String param = args[0];
+
+        switch (param) {
+            case CMD_HELP: displayHelp();
+            case CMD_ABOUT: displayAbout();
+            case CMD_VERSION: displayVersion();
+            default: displayError();
+        }
+    }
+
+    private static void displayWelcome() {
+        System.out.println("* WELCOME TO TASK MANAGER *");
+    }
+
+
+    private static void  displayAbout() {
+        System.out.println("Gasanov Asiman");
+        System.out.println("gasanov_ad@nlmk.com");
+        System.exit(0);
+    }
+
+    private static  void displayError() {
+        System.out.println("ERROR! You passed wrong parameters ");
+        System.exit(-1);
+    }
+
+    private static void  displayHelp() {
+        System.out.println("version - Display program version.");
+        System.out.println("about - Display programm info.");
+        System.out.println("help - Display list of terminal commands.");
+        System.exit(0);
+    }
+
+    private static void  displayVersion() {
+        System.out.println("1.0.0");
+        System.exit(0);
+    }
+
 }
---
+++ src/main/java/ru/gasanov/tm/constant/TerminalConst.java
@@ -0,0 +1,7 @@
+package ru.gasanov.tm.constant;
+
+public class TerminalConst {
+    public static final String CMD_HELP = "help";
+    public static final String CMD_VERSION = "version";
+    public static final String CMD_ABOUT = "about";
+}
